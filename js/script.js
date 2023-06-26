75fa1a430a5b2169129037b2b8fab8ebf426fb1b
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2023-06-26 10:29:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 
+//ScrollReveal
 window.sr = ScrollReveal({ reset: true });
-
+//
 sr.reveal('.area', {
     rotate: { x: 100, y: 100, z: 0},
     duration: 2000
+});
+
+//
+var slideUp = {
+  distance: '100%',
+  origin: 'bottom',
+  opacity: null,
+  delay: 250,
+  duration: 500,
+  reset: true
+}
+
+ScrollReveal().reveal('.slide-up', {
+  distance: '200%',
+  origin: 'bottom',
+  opacity: '0',
+  delay: 250,
+  duration: 250,
+  reset: true
+})
+
+//
+ScrollReveal().reveal('.item', { delay: 250 });
+ScrollReveal().reveal('.item', { duration: 500 });
+ScrollReveal().reveal('.item', { delay: 375, reset: true });
+
+ScrollReveal().reveal('.item', {
+  delay: 375,
+  duration: 500,
+  reset: true
 });
