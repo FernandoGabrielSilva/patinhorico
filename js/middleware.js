1357bb121acc718b97362c1e33f9f4daa68ddb0d
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2024-01-02 15:50:01 +0000

    Delete js directory

--- js/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { get } from '@vercel/edge-config';
-
-export const config = { matcher: '/welcome' };
-
-export function middleware() {
-  const greeting = await get('greeting');
-  return NextResponse.json(greeting);
-}
