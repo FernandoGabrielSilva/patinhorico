09df6fdd5e6c9fc38644503e51fef9fd348494ae
Fernando Gabriel Silva <107934284+FernandoGabrielSilva@users.noreply.github.com>
2024-01-02 12:22:27 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+//Spam
+
+let span1 = document.querySelector(".span-1")
+let span2 = document.querySelector(".span-2")
+let span3 = document.querySelector(".span-3")
 
 //ScrollReveal
 
@@ -9,3 +14,26 @@ ScrollReveal().reveal('.slide-up', {
   duration: 250,
   reset: true
 })
+
+
+//ANIMATION SPAM
+
+function displaySpan(message) {
+  for (let i = 0; i <= 10; i++) {
+    setTimeout(function(nr) {
+      span1.innerHTML = message + i;
+    }, i * 3000 / 10, i)
+  }
+  for (let i = 0; i <= 13; i++) {
+    setTimeout(function(nr) {
+      span2.innerHTML = message + i;
+    }, i * 3000 / 13, i)
+  }
+  for (let i = 0; i <= 400; i++) {
+    setTimeout(function(nr) {
+      span3.innerHTML = message + i;
+    }, i * 3000 / 400, i)
+  }
+}
+
+displaySpan("+");
